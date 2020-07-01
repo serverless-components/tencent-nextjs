b4188818df65257d09b435fe86395dce8d3c4325
Avril Li <linxforever@vip.qq.com>
2020-07-01 15:13:29 +0800

    chore: update test case

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -4,7 +4,7 @@ const path = require('path')
 const axios = require('axios')
 
 // set enough timeout for deployment to finish
-jest.setTimeout(50000)
+jest.setTimeout(300000)
 
 // the yaml file we're testing against
 const instanceYaml = {
@@ -14,7 +14,9 @@ const instanceYaml = {
   name: `nextjs-integration-tests-${generateId()}`,
   stage: 'dev',
   inputs: {
-    // region: 'ap-guangzhou'
+    region: 'ap-guangzhou',
+    runtime: 'Nodejs8.9',
+    apigatewayConf: { environment: 'test' }
   }
 }
 
@@ -26,41 +28,18 @@ const credentials = {
 // get serverless construct sdk
 const sdk = getServerlessSdk(instanceYaml.org)
 
-// clean up the instance after tests
-afterAll(async () => {
-  await sdk.remove(instanceYaml, credentials)
-})
-
 it('should successfully deploy nextjs app', async () => {
   const instance = await sdk.deploy(instanceYaml, { tencent: {} })
   expect(instance).toBeDefined()
   expect(instance.instanceName).toEqual(instanceYaml.name)
+  expect(instance.outputs).toBeDefined()
   // get src from template by default
   expect(instance.outputs.templateUrl).toBeDefined()
-})
-
-it('should successfully update basic configuration', async () => {
-  instanceYaml.inputs.region = 'ap-shanghai'
-  instanceYaml.inputs.runtime = 'Nodejs8.9'
-  instanceYaml.inputs.functionName = 'nextjsDemoTest'
-
-  const instance = await sdk.deploy(instanceYaml, credentials)
-
-  expect(instance.outputs).toBeDefined()
   expect(instance.outputs.region).toEqual(instanceYaml.inputs.region)
   expect(instance.outputs.scf).toBeDefined()
   expect(instance.outputs.scf.runtime).toEqual(instanceYaml.inputs.runtime)
-  expect(instance.outputs.scf.functionName).toEqual(instanceYaml.inputs.functionName)
-})
-
-it('should successfully update apigatewayConf', async () => {
-  instanceYaml.inputs.apigatewayConf = { environment: 'test' }
-  const instance = await sdk.deploy(instanceYaml, credentials)
-
-  expect(instance.outputs).toBeDefined()
   expect(instance.outputs.apigw).toBeDefined()
   expect(instance.outputs.apigw.environment).toEqual(instanceYaml.inputs.apigatewayConf.environment)
-
 })
 
 it('should successfully update source code', async () => {
@@ -76,18 +55,9 @@ it('should successfully update source code', async () => {
   expect(response.data.includes('Test Nextjs')).toBeTruthy()
 })
 
-it('should successfully disable auto create api gateway', async () => {
-  instanceYaml.inputs.apigatewayConf = { isDisabled: true }
-  const instance = await sdk.deploy(instanceYaml, credentials)
-
-  expect(instance.outputs).toBeDefined()
-  expect(instance.outputs.apigw).not.toBeDefined()
-})
-
 it('should successfully remove nextjs app', async () => {
   await sdk.remove(instanceYaml, credentials)
   result = await sdk.getInstance(instanceYaml.org, instanceYaml.stage, instanceYaml.app, instanceYaml.name)
 
-  // remove action won't delete the service cause the apigw have the api binded
   expect(result.instance.instanceStatus).toEqual('inactive')
 })
