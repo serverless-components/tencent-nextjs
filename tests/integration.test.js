44dbc1aa337ea859d8fa34d9dd76878c6283dc71
Avril Li <linxforever@vip.qq.com>
2020-07-03 15:05:20 +0800

    Add integration test (#7)
    * test: add integration test
    
    * fix: update test function name
    
    * chore: update jest version to support node8.x
    
    * chore: remove process.env variables of test
    
    * chore: update test case
    
    * chore: update travis runtime node version
    
    * chore: update test region and ignore config
    
    * chore: update test timeout config
    
    * chore: update test case

---
+++ tests/integration.test.js
@@ -0,0 +1,54 @@
+const { generateId, getServerlessSdk } = require('./utils')
+const execSync = require('child_process').execSync
+const path = require('path')
+const axios = require('axios')
+
+// set enough timeout for deployment to finish
+jest.setTimeout(600000)
+
+// the yaml file we're testing against
+const instanceYaml = {
+  org: 'orgDemo',
+  app: 'appDemo',
+  component: 'nextjs@dev',
+  name: `nextjs-integration-tests-${generateId()}`,
+  stage: 'dev',
+  inputs: {
+    region: 'ap-guangzhou',
+    runtime: 'Nodejs10.15',
+    apigatewayConf: { environment: 'test' }
+  }
+}
+
+// get credentials from process.env but need to init empty credentials object
+const credentials = {
+  tencent: {}
+}
+
+// get serverless construct sdk
+const sdk = getServerlessSdk(instanceYaml.org)
+
+it('should successfully deploy nextjs app', async () => {
+  const instance = await sdk.deploy(instanceYaml, { tencent: {} })
+
+  expect(instance).toBeDefined()
+  expect(instance.instanceName).toEqual(instanceYaml.name)
+  expect(instance.outputs).toBeDefined()
+  // get src from template by default
+  expect(instance.outputs.templateUrl).toBeDefined()
+  expect(instance.outputs.region).toEqual(instanceYaml.inputs.region)
+  expect(instance.outputs.scf).toBeDefined()
+  expect(instance.outputs.scf.runtime).toEqual(instanceYaml.inputs.runtime)
+  expect(instance.outputs.apigw).toBeDefined()
+  expect(instance.outputs.apigw.environment).toEqual(instanceYaml.inputs.apigatewayConf.environment)
+
+  const response = await axios.get(instance.outputs.apigw.url)
+  expect(response.data.includes('Next.js!')).toBeTruthy()
+})
+
+it('should successfully remove nextjs app', async () => {
+  await sdk.remove(instanceYaml, credentials)
+  result = await sdk.getInstance(instanceYaml.org, instanceYaml.stage, instanceYaml.app, instanceYaml.name)
+
+  expect(result.instance.instanceStatus).toEqual('inactive')
+})
