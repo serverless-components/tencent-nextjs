b2f784d956a69c3b9145c24886d1bad2b2d5e51d
yugasun <yuga_sun@163.com>
2020-10-19 14:58:41 +0800

    chore: update example

--- example/next.config.js
+++ example/next.config.js
@@ -1,3 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  assetPrefix: ''
+  env: {
+    STATIC_URL: isProd ? process.env.STATIC_URL : ''
+  },
+  assetPrefix: isProd ? process.env.STATIC_URL : ''
 }
--- example/pages/index.js
+++ example/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Link from 'next/link'
 
 const Home = () => (
   <div className="container">
@@ -9,24 +8,18 @@ const Home = () => (
     </Head>
 
     <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+      <h1 className="title">Welcome to Next.js!</h1>
 
       <p className="description">
         Get started by editing <code>pages/index.js</code>
       </p>
+      <p className="description">
+        The SSR app is hosted on{' '}
+        <a href="https://cloud.tencent.com/product/ssr" target="_blank" rel="noopener noreferrer">
+          Serverless SSR
+        </a>
+      </p>
     </main>
-
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
   </div>
 )
 
