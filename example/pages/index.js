0a286c2c694ec37f1716aa51946e49ae5b2a30e2
Avril Li <linxforever@vip.qq.com>
2021-07-14 20:27:39 +0800

    chore: nextjs example upgrade

--- example/pages/index.js
+++ example/pages/index.js
@@ -1,26 +1,27 @@
 import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href={`${process.env.STATIC_URL}/favicon.ico`} />
-    </Head>
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href={`${process.env.STATIC_URL || ''}/favicon.ico`} />
+      </Head>
 
-    <main>
-      <h1 className="title">Welcome to Next.js!</h1>
+      <main>
+        <h1 className={styles.title}>Welcome to Next.js!</h1>
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-      <p className="description">
-        The SSR app is hosted on{' '}
-        <a href="https://cloud.tencent.com/product/ssr" target="_blank" rel="noopener noreferrer">
-          Serverless SSR
-        </a>
-      </p>
-    </main>
-  </div>
-)
-
-export default Home
+        <p className={styles.description}>
+          Get started by editing <code>pages/index.js</code>
+        </p>
+        <p className={styles.description}>
+          The SSR app is hosted on&nbsp;
+          <a href="https://cloud.tencent.com/product/ssr" target="_blank" rel="noopener noreferrer">
+            Serverless SSR
+          </a>
+        </p>
+      </main>
+    </div>
+  )
+}
